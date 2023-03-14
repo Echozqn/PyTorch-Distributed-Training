69d28fe85dcde6af26676197e686385284a709df
Echozqn <50656301+Echozqn@users.noreply.github.com>
2023-03-14 14:19:37 +0800

    update main.py

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ from torch.utils.data.distributed import DistributedSampler
 
 def reduce_loss(tensor, rank, world_size):
     with torch.no_grad():
-        dist.reduce(tensor, dst=0)
+        dist.reduce(tensor, dst=0) # 与其他worker进行同步
         if rank == 0:
             tensor /= world_size
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--local_rank', type=int, help="local gpu id")
+parser.add_argument('--batch_size', default=128, type=int, help="batch size")
+
 args = parser.parse_args()
-world_size = 2
-batch_size = 128
-epochs = 5
+world_size = dist.get_world_size()
+batch_size = args.batch_size
+epochs = 2
 lr = 0.001
 
 dist.init_process_group(backend='nccl', init_method='env://')
@@ -63,6 +65,9 @@ val_loader = DataLoader(valset,
                         pin_memory=True)
 
 
+file_name = f"{batch_size}_{global_rank}.log"
+data_file = open(file_name, "w")
+data_file.write("datetime\tg_step\tg_img\tloss_value\texamples_per_sec\n")
 import torch.profiler
 with torch.profiler.profile(
         schedule=torch.profiler.schedule(wait=2, warmup=2, active=6, repeat=1),
@@ -80,6 +85,8 @@ with torch.profiler.profile(
     net = DDP(net, device_ids=[args.local_rank], output_device=args.local_rank)
     net.train()
     print("Start")
+    global_step = 0
+    train_begin = time.time()
     for e in range(epochs):
         # DistributedSampler deterministically shuffle data
         # by seting random seed be current number epoch
@@ -87,26 +94,40 @@ with torch.profiler.profile(
         # the order of shuffled data will be always same
         sampler.set_epoch(e)
         for idx, (imgs, labels) in enumerate(train_loader):
-            imgs = imgs.cuda()
-            labels = labels.cuda()
-            output = net(imgs)
+            start = time.time()
+
+            global_step += 1
+            imgs = imgs.cuda() # loading
+            labels = labels.cuda() #loading
+            output = net(imgs) # running
             loss = criterion(output, labels)
             opt.zero_grad()
             loss.backward()
             opt.step()
             reduce_loss(loss, global_rank, world_size)
+
+            if global_rank == 0 and global_step % 5 == 0:
+                duration = time.time() - start
+                examples_per_sec = batch_size / duration
+                val = f"{datetime.now()}\t{global_step * world_size}\t{global_step * world_size * batch_size}\t{loss.item()}\t{examples_per_sec}\n"
+                data_file.write(val)
+
             if idx % 10 == 0 and global_rank == 0:
                 print('Epoch: {} step: {} loss: {}'.format(e, idx, loss.item()))
             p.step()
-    net.eval()
-    with torch.no_grad():
-        cnt = 0
-        total = len(val_loader.dataset)
-        for imgs, labels in val_loader:
-            imgs, labels = imgs.cuda(), labels.cuda()
-            output = net(imgs)
-            predict = torch.argmax(output, dim=1)
-            cnt += (predict == labels).sum().item()
 
-    if global_rank == 0:
-        print('eval accuracy: {}'.format(cnt / total))
+        data_file.write("TrainTime\t%f\n" % (time.time() - train_begin))
+
+        
+    # net.eval()
+    # with torch.no_grad():
+    #     cnt = 0
+    #     total = len(val_loader.dataset)
+    #     for imgs, labels in val_loader:
+    #         imgs, labels = imgs.cuda(), labels.cuda()
+    #         output = net(imgs)
+    #         predict = torch.argmax(output, dim=1)
+    #         cnt += (predict == labels).sum().item()
+
+    # if global_rank == 0:
+    #     print('eval accuracy: {}'.format(cnt / total))
